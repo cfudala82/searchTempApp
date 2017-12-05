6cc9874d0eccbcd6680e8fca9661fa7930f9bd8a
Christopher Fudala <cfudala82@me.com>
2017-12-05 13:50:00 -0600

    changed actions reducers and containers/search_bar

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,27 @@
 import axios from 'axios';
 
-const API_KEY = '';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const API_KEY = 'f9725c2ea02928f8096cee473f9f9658';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
-export funcion fetchWeather(city) {
+export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
+  // request
+  //   .then(function (response) {
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     console.error(error);
+  //   });
+  console.log('Request:', request);
 
   return {
     type: FETCH_WEATHER,
     payload: request
   }
 }
+
+
+// https://api.openweathermap.org/data/2.5/forecast?appid=f9725c2ea02928f8096cee473f9f9658&q=chicago,us
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = { term: '' };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -14,9 +18,12 @@ export default class SearchBar extends Component {
   }
 
   onFormSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     // We need to fetch weather data
+    this.props.fetchWeather(this.state.term);
+    // clear out search input
+    this.setState({term: ''});
   }
 
   render() {
@@ -35,3 +42,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
