8245730b074090a00bf3ded4b212b425aed41163
Christopher Fudala <cfudala82@me.com>
2017-12-05 20:48:22 -0600

    addded chart.js, google_map.js, modified actions/index, app.js, style.css and weather_list.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,20 +8,9 @@ export const FETCH_WEATHER = 'FETCH_WEATHER'
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
-  // request
-  //   .then(function (response) {
-  //     console.log(response);
-  //   })
-  //   .catch(function (error) {
-  //     console.error(error);
-  //   });
-  console.log('Request:', request);
 
   return {
     type: FETCH_WEATHER,
     payload: request
   }
 }
-
-
-// https://api.openweathermap.org/data/2.5/forecast?appid=f9725c2ea02928f8096cee473f9f9658&q=chicago,us
